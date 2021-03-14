1cb3ff1fd0bcb09eb3eedc2b613451a37e445667
bigurus <breicgleason@gmail.com>
2021-03-14 13:46:23 +0000

    adding 99 bottles of beer function

---
+++ mypython.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+impor
+
+def countdown_bottles(y):
+    for i in range(y,0,-1):
+        print(f'{i} bottles of beer on the wall')
+        print(f'{i} bottles of beer')
+        print(f'Take one down, pass it around,')
+        print(f'{i} bottles of beer on the wall')
+
+countdown_bottles(10)
+
