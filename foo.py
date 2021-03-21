4f45b67aef9087af7fec4528cc1269adc59fa6d3
Seirid <koltegamer@gmail.com>
2021-03-21 13:33:30 +0000

    Added "main" function

--- foo.py
+++ foo.py
@@ -8,14 +8,21 @@ CHAR_STATS = {
         'int' : 1,
         'con' : 0,
         'agi' : 0,
-        'app' : 0
+        'app' : 1
     },
     'fighter' : {
         'str' : 1,
         'int' : -1,
         'con' : 1,
         'agi' : 0,
-        'app' : 1
+        'app' : 0
+    },
+    'rogue' : {
+         'str' : 0,
+         'int' : 1,
+         'con' : -1,
+         'agi' : 1,
+         'app' : 0
     }
 }
 
@@ -32,4 +39,33 @@ def roll_character():
     }
 
 def char_stats(stat,char_class):
-    CHAR_STATS[char_class][stat]
+    return CHAR_STATS[char_class][stat]
+
+def class_name(class_number):
+    if class_number=="1":
+        return "mage"
+    elif class_number=="2":
+        return "fighter"
+    elif class_number=="3":
+        return "rogue"
+    else:
+        raise ValueError("Unknown class number")
+
+def main():
+    print("Hello, you are playing ULTIMATE SUPER ARENA DEATH MATCH")
+    print("What is the name of your character?:")
+    name=input()
+    cls=None
+    while cls==None:
+        print("What is your class? Choose a number:")
+        print("1. Mage 2. Fighter 3. Rogue")
+        cls=input()
+        if cls in ["1","2","3"]:
+            pass
+        else:
+            cls=None
+            print("That is not a valid character class!")
+    print(f"Welcome, {name}")
+    cls_name=class_name(cls)
+    print(f"Welcome to the guild of {cls_name}s")
+    
